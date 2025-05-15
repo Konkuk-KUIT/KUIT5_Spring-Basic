3f37e2eaa0b7e192bf6a24b8cd1090cf4f4ffc32
sudo-Terry <contact.terrykim@gmail.com>
2025-05-15 13:43:53 +0900

    feat: QnaController 구현

--- src/main/java/kuit/springbasic/controller/qna/QnaController.java
+++ src/main/java/kuit/springbasic/controller/qna/QnaController.java
@@ -1,9 +1,31 @@
 package kuit.springbasic.controller.qna;
 
+import kuit.springbasic.db.QuestionRepository;
+import kuit.springbasic.domain.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class QnaController {
+    private final QuestionRepository questionRepository;
+
+    @Autowired
+    public QnaController(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
 
     /**
      * TODO: showQnA
      */
+    @RequestMapping("/qna/show")
+    public String showQuestionForm(@RequestParam("questionId") int questionId, Model model) {
+        Question question = questionRepository.findByQuestionId(questionId);
+        model.addAttribute("question", question);
+
+        return "qna/show";
+    }
 
 }
