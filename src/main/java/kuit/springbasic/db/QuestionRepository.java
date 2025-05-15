56d1df5a14d04c6e50d145f52733f1a81c646426
dlstkdals123 <dlstkdals123@naver.com>
2025-05-15 20:38:59 +0900

    Refactor: 안쓰는 import문 제거

--- src/main/java/kuit/springbasic/db/QuestionRepository.java
+++ src/main/java/kuit/springbasic/db/QuestionRepository.java
@@ -1,7 +1,6 @@
 package kuit.springbasic.db;
 
 import kuit.springbasic.domain.Question;
-import kuit.springbasic.domain.User;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
