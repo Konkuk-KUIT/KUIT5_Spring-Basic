a20e0a20c3b96e61b66af8a92d493e4f6b81e13a
Minju <angertiger0941@gmail.com>
2025-05-15 16:51:00 +0900

    showHomeV1 without DI

--- src/main/java/kuit/springbasic/controller/HomeController.java
+++ src/main/java/kuit/springbasic/controller/HomeController.java
@@ -1,12 +1,31 @@
 package kuit.springbasic.controller;
 
-public class HomeController {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import kuit.springbasic.db.MemoryQuestionRepository;
+import kuit.springbasic.db.MemoryUserRepository;
+import kuit.springbasic.db.QuestionRepository;
+import kuit.springbasic.db.UserRepository;
+import kuit.springbasic.domain.Question;
+import kuit.springbasic.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Collection;
+
+@Controller
+public class HomeController {
+    private QuestionRepository questionRepository = new MemoryQuestionRepository();
     /**
      * TODO: showHome
      * showHomeV1 : parameter - HttpServletRequest, HttpServletResponse / return - ModelAndView
      * showHomeV2 : parameter - none / return - ModelAndView
      * showHomeV3 : parameter - Model / return - String
      */
-
+    @RequestMapping("/homeV1")
+    public ModelAndView showHomeV1(HttpServletRequest request, HttpServletResponse response){
+        Collection<Question> questions = questionRepository.findAll();
+        return new ModelAndView("home").addObject("questions", questions);
+    }
 }
