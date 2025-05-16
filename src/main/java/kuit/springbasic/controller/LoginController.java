8a0c6e3c5f0706beb35c1a590b009cdb7f456660
Minju <angertiger0941@gmail.com>
2025-05-16 21:12:37 +0900

    chore: remove unused import

--- src/main/java/kuit/springbasic/controller/LoginController.java
+++ src/main/java/kuit/springbasic/controller/LoginController.java
@@ -5,9 +5,7 @@ import jakarta.servlet.http.HttpSession;
 import kuit.springbasic.db.UserRepository;
 import kuit.springbasic.domain.User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
