417df931ada5862409929ae40f36cb4cea642288
Minju <angertiger0941@gmail.com>
2025-05-16 20:25:23 +0900

    Home

--- src/main/java/kuit/springbasic/controller/HomeController.java
+++ src/main/java/kuit/springbasic/controller/HomeController.java
@@ -1,18 +1,9 @@
 package kuit.springbasic.controller;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import kuit.springbasic.db.MemoryQuestionRepository;
-import kuit.springbasic.db.MemoryUserRepository;
 import kuit.springbasic.db.QuestionRepository;
-import kuit.springbasic.db.UserRepository;
 import kuit.springbasic.domain.Question;
-import kuit.springbasic.domain.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Collection;
 
@@ -31,23 +22,32 @@ public class HomeController {
      * showHomeV2 : parameter - none / return - ModelAndView
      * showHomeV3 : parameter - Model / return - String
      */
-    @RequestMapping("/homeV1")
-    public ModelAndView showHomeV1(HttpServletRequest request, HttpServletResponse response){
-        Collection<Question> questions = questionRepository.findAll();
-        return new ModelAndView("home").addObject("questions", questions);
-    }
+//    @RequestMapping("/homeV1")
+//    public ModelAndView showHomeV1(HttpServletRequest request, HttpServletResponse response){
+//        Collection<Question> questions = questionRepository.findAll();
+//        return new ModelAndView("home").addObject("questions", questions);
+//    }
+//
+//    @RequestMapping("/homeV2")
+//    public ModelAndView showHomeV2(){
+//        Collection<Question> questions = questionRepository.findAll();
+//        return new ModelAndView("home").addObject("questions", questions);
+//    }
 
-    @RequestMapping("/homeV2")
-    public ModelAndView showHomeV2(){
-        Collection<Question> questions = questionRepository.findAll();
-        return new ModelAndView("home").addObject("questions", questions);
-    }
-
-    @RequestMapping("/homeV3")
+//    @RequestMapping("/homeV3")
+    @RequestMapping("/home")
     public String showHomeV3(Model model) {
         Collection<Question> questions = questionRepository.findAll();
         model.addAttribute("questions", questions);
 
         return "home";
     }
+
+//    @RequestMapping("/homeV4")
+//    public ModelAndView showHomeV4(Model model) {
+//        Collection<Question> questions = questionRepository.findAll();
+//        model.addAttribute("questions", questions);
+//
+//        return new ModelAndView("home");
+//    }
 }
