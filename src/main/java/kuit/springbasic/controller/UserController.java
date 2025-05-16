cfa2fcba331b600bde5fbd2ec3518cc41cf8e18a
임유리 <dladbfl67@naver.com>
2025-05-16 23:34:05 +0900

    요구사항 1

--- src/main/java/kuit/springbasic/controller/UserController.java
+++ src/main/java/kuit/springbasic/controller/UserController.java
@@ -1,32 +1,59 @@
 package kuit.springbasic.controller;
 
-import lombok.NoArgsConstructor;
+import jakarta.servlet.http.HttpServletRequest;
+import kuit.springbasic.db.UserRepository;
+import kuit.springbasic.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@NoArgsConstructor
+import java.util.Collection;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
 
-    /**
-     * TODO: showUserForm
-     */
+    private final UserRepository userRepository;
 
-    /**
-     * TODO: createUser
-     * createUserV1 : @RequestParam
-     * createUserV2 : @ModelAttribute
-     */
+    @GetMapping("/form")
+    public String showUserForm() {
+        return "user/form";
+    }
 
-    /**
-     * TODO: showUserList
-     */
+    @PostMapping("/create")
+    public String createUser(@RequestParam("userId") String userId,
+                             @RequestParam("password") String password,
+                             @RequestParam("name") String name,
+                             @RequestParam("email") String email) {
+        User user = new User(userId, password, name, email);
+        userRepository.insert(user);
+        return "redirect:/homeV2";
+    }
 
-    /**
-     * TODO: showUserUpdateForm
-     */
+    @GetMapping
+    public String showUserList(Model model) {
+        Collection<User> users = userRepository.findAll();
+        model.addAttribute("users", users);
+        return "user/list";
+    }
 
-    /**
-     * TODO: updateUser
-     * updateUserV1 : @RequestParam
-     * updateUserV2 : @ModelAttribute
-     */
+    @GetMapping("/updateForm")
+    public String showUserUpdateForm(HttpServletRequest request, Model model) {
+        String userId = request.getParameter("userId");
+        User user = userRepository.findByUserId(userId);
+        model.addAttribute("user", user);
+        return "user/updateForm";
+    }
 
+    @PostMapping("/update")
+    public String updateUser(@RequestParam("userId") String userId,
+                             @RequestParam("password") String password,
+                             @RequestParam("name") String name,
+                             @RequestParam("email") String email) {
+        User user = new User(userId, password, name, email);
+        userRepository.update(user);
+        return "redirect:/users";
+    }
 }
