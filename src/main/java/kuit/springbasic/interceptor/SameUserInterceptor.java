d13050ea774805f33a9a5f2eca63d3fb4ee82e70
hamhyeongju <gudwn357@gmail.com>
2025-05-17 14:09:38 +0900

    refactor: 불필요한 redirect prefix 값 제거

--- src/main/java/kuit/springbasic/interceptor/SameUserInterceptor.java
+++ src/main/java/kuit/springbasic/interceptor/SameUserInterceptor.java
@@ -20,12 +20,12 @@ public class SameUserInterceptor implements HandlerInterceptor {
 
         // 2. 파라미터에 값이 존재하지 않으면 redirect
         if (!StringUtils.hasText(userId)) {
-            response.sendRedirect("redirect:/");
+            response.sendRedirect("/");
             return false;
         }
 
         // 3. 세션에서 현재 로그인된 사용자를 가져옴
-        HttpSession session = request.getSession(false); // 세션이 없으면 null 반환
+        HttpSession session = request.getSession();
         User user = (User) session.getAttribute(UserSessionUtils.USER_SESSION_KEY);
 
         // 4. 모든 검증을 통과하면 요청을 계속 진행
