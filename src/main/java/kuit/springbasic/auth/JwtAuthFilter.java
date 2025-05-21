79ec36d174555a45f0642981c723013ef1bef0cc
woo319 <132249641+woo319@users.noreply.github.com>
2025-05-22 01:57:23 +0900

    1-1

---
+++ src/main/java/kuit/springbasic/auth/JwtAuthFilter.java
@@ -0,0 +1,43 @@
+package kuit.springbasic.auth;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtAuthFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String token = resolveToken(request);
+
+        String loginUserId = (token != null) ? jwtTokenProvider.validateToken(token) : null;
+
+        if (loginUserId != null) {
+            request.setAttribute("loginUserId", loginUserId);
+        }
+
+        //다음필터로 넘어가기
+        filterChain.doFilter(request, response);
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7); // "Bearer " 뒷부분만 추출
+        }
+
+        return null;
+    }
+}
--- src/main/java/kuit/springbasic/controller/AuthController.java
+++ src/main/java/kuit/springbasic/controller/AuthController.java
@@ -7,6 +7,7 @@ import kuit.springbasic.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,6 +26,7 @@ public class AuthController {
 
     // 일반 인가 범위
     @GetMapping("/auth")
+    @ResponseBody
     public String auth() {
         return "ok";
     }
@@ -32,6 +34,7 @@ public class AuthController {
     // 인터셉터를 통한 loginUserId
     // 응답 결과로 "ok, loginUserId = kuit" 이 나와야 함
     @GetMapping("/auth/userId")
+    @ResponseBody
     public String authUserId(@RequestParam("loginUserId") String loginUserId) {
         return "ok, loginUserId = " + loginUserId;
     }
